a4824dd1ac4f013fc6c52c44fafd26b6cc48cdc0
Benjamin Cooper <bencooper222@gmail.com>
2019-02-21 22:13:41 -0600

    Simplified and shrunk bundle

--- parsing/src/ugg.js
+++ parsing/src/ugg.js
@@ -4,90 +4,85 @@ import {
   openWebpage,
   displayResults,
 } from './common.js';
-
 import * as defaultKeyNames from '../../defaultKeyNames.json';
 
-const objectReduce = require('lodash.reduce');
-// import * as lodashReduce from 'lodash.reduce';
-
 (async () => {
-  const prepSite = () => {
-    return new Promise((res, rej) => {
-      let oldLen = document.getElementsByClassName('rt-tr-group').length;
-      let newLen = 0;
-      let attempts = 0;
+  // prepare site by force loading table
+  await new Promise(res => {
+    let oldLen = document.getElementsByClassName('rt-tr-group').length,
+      newLen = 0,
+      attempts = 0;
 
-      const interval = setInterval(() => {
-        window.scrollTo(0, document.body.scrollHeight);
-        oldLen = newLen;
-        newLen = document.getElementsByClassName('rt-tr-group').length;
+    const interval = setInterval(() => {
+      window.scrollTo(0, document.body.scrollHeight);
+      oldLen = newLen;
+      newLen = document.getElementsByClassName('rt-tr-group').length;
 
-        if ((oldLen === newLen && newLen >= 167) || attempts++ > 7) {
-          clearInterval(interval);
-          res();
-        }
-      }, 20);
-    });
-  };
+      const LIST_LENGTH = 167; // loosely update with length of u.gg list every so often
+      if (
+        (oldLen === newLen && newLen >= LIST_LENGTH) ||
+        attempts++ > Math.ceil((2 * LIST_LENGTH) / 50)
+      ) {
+        clearInterval(interval);
+        res();
+      }
+    }, 20);
+  });
 
-  const getAndScrapeTable = () => {
-    const table = Array.from(
-      document.getElementsByClassName('rt-tbody')[0].childNodes,
+  // restore usability asap
+  document.body.scrollTop = document.documentElement.scrollTop = 0; // https://www.youtube.com/watch?v=bmMhAMnrJDA
+
+  // gather data
+  const champDataObject = Array.from(
+    document.getElementsByClassName('rt-tbody')[0].childNodes,
+  ).reduce((champData, row) => {
+    const rowInfo = row.childNodes[0];
+    const name = rowInfo.childNodes[2].getElementsByClassName(
+      'champion-name',
+    )[0].innerHTML;
+
+    const gamesPlayed = Number(
+      rowInfo.childNodes[8]
+        // @ts-ignore
+        .getElementsByTagName('span')[0]
+        .innerHTML.replace(',', '')
+        .replace(',', ''), // there's only going to be max two commas...
+    );
+    // console.log(gamesPlayed);
+    const winPercent = parsePercent(
+      // @ts-ignore
+      rowInfo.childNodes[4].getElementsByTagName('b')[0].innerHTML,
+      true,
     );
 
-    return table.reduce((champData, row) => {
-      const rowInfo = row.childNodes[0];
-      let name = rowInfo.childNodes[2].getElementsByClassName(
-        'champion-name',
-      )[0].innerHTML;
+    if (champData[name] == null) {
+      champData[name] = {
+        played: gamesPlayed,
+        won: gamesPlayed * winPercent,
+      };
+    } else {
+      champData[name].played += gamesPlayed;
+      champData[name].won += gamesPlayed * winPercent;
+    }
+    return champData;
+  }, {});
 
-      const gamesPlayed = Number(
-        rowInfo.childNodes[8]
-          // @ts-ignore
-          .getElementsByTagName('span')[0]
-          .innerHTML.replace(',', '')
-          .replace(',', ''), // there's only going to be max two commas...
-      );
-      // console.log(gamesPlayed);
-      const winPercent = parsePercent(
-        // @ts-ignore
-        rowInfo.childNodes[4].getElementsByTagName('b')[0].innerHTML,
-        true,
-      );
-
-      if (champData[name] == null) {
-        champData[name] = {
-          played: gamesPlayed,
-          won: gamesPlayed * winPercent,
-        };
-      } else {
-        champData[name].played += gamesPlayed;
-        champData[name].won += gamesPlayed * winPercent;
-      }
-      return champData;
-    }, {});
-  };
-
-  await prepSite();
-  const champDataObject = getAndScrapeTable();
-
-  const totalGames = objectReduce(
-    champDataObject,
-    (result, value) => {
-      return value.played + result;
-    },
+  const totalGames = Object.keys(champDataObject).reduce(
+    (acc, champName) => champDataObject[champName].played + acc,
     0,
   );
 
+  // manipulate data into proper format
   const championDataArray = Object.keys(champDataObject)
     .reduce((acc, champName) => {
-      const champ = champDataObject[champName];
+      const { won, played } = champDataObject[champName];
+
       return acc.concat({
-        [defaultKeyNames.WIN_PERCENT]: champ.won / champ.played,
-        [defaultKeyNames.PLAY_PERCENT]: (100 * champ.played) / totalGames,
+        [defaultKeyNames.WIN_PERCENT]: won / played,
+        [defaultKeyNames.PLAY_PERCENT]: (100 * played) / totalGames,
         [defaultKeyNames.POWER]: calculatePower(
-          champ.won / champ.played,
-          (100 * champ.played) / totalGames,
+          won / played,
+          (100 * played) / totalGames,
         ),
         [defaultKeyNames.NAME]: champName,
       });
@@ -96,5 +91,4 @@ const objectReduce = require('lodash.reduce');
 
   displayResults(championDataArray);
   openWebpage(championDataArray);
-  document.body.scrollTop = document.documentElement.scrollTop = 0; // https://www.youtube.com/watch?v=bmMhAMnrJDA
 })();
